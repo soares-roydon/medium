908afccf96ddc9beda78ceb5b084f2edff45f97d
soares-roydon <soaresroydon@gmail.com>
2026-05-28 22:05:14 +0530

    Add prisma orm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,25 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono().basePath("/api/v1");
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/signup", (c) => {
+  return c.json({});
+});
 
-export default app
+app.post("/signin", (c) => {
+  return c.json({});
+});
+
+app.post("/blog", (c) => {
+  return c.json({});
+});
+
+app.put("/blog", (c) => {
+  return c.json({});
+});
+
+app.get("/blog/:id", (c) => {
+  return c.json({});
+});
+
+export default app;

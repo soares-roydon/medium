2b3d6f430029c758ef54310f9a86882c413475f0
soares-roydon <soaresroydon@gmail.com>
2026-06-02 23:03:03 +0530

    Add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { userRouter } from "./routes/user";
 import { blogRouter } from "./routes/blog";
 
 const app = new Hono().basePath("/api/v1");
+
+app.use("/api/v1/*", cors())
 
 app.route("/user", userRouter)
 app.route("/blog", blogRouter)
